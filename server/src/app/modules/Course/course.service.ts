722b0d1b3b642e45a509a45853749f217d96be16
raihanwebmaster <raihanemon2015@gmail.com>
2024-06-24 15:37:07 +0200

    update cacheing

--- server/src/app/modules/Course/course.service.ts
+++ server/src/app/modules/Course/course.service.ts
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { destroyImage } from '../../utils/deleteImageIntoCloudinary';
+import { redis } from '../../utils/redis';
 import { sendImageToCloudinary } from '../../utils/sendImageToCloudinary';
 import { Course } from './course.model';
 import cloudinary from 'cloudinary';
@@ -39,12 +40,22 @@ const updateCourseFromDB = async (couserId: string, course: any) => {
 };
 
 const getCourseWithOutPurchaseingFromDB = async (courseId: string) => {
+    const isCacheExist = await redis.get(courseId);
+    if (isCacheExist) {
+        return JSON.parse(isCacheExist);
+    }
     const courses = await Course.findById(courseId).select("-courseData.videoUrl  -courseData.videoPlayer -courseData.links -courseData.suggestion -courseData.questions");
+    await redis.set(courseId, JSON.stringify(courses));
     return courses;
 };
 
 const getAllCoursesWithOutPurchaseingFromDB = async () => {
+    const isCacheExist = await redis.get("courses");
+    if (isCacheExist) {
+        return JSON.parse(isCacheExist);
+    }
     const courses = await Course.find().select("-courseData.videoUrl  -courseData.videoPlayer -courseData.links -courseData.suggestion -courseData.questions");
+    await redis.set("courses", JSON.stringify(courses));
     return courses;
 };
 
