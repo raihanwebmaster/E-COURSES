753ab583046c4bc422fcb289357fcb34e67c88d5
raihanwebmaster <raihanemon2015@gmail.com>
2024-06-27 10:16:50 +0200

    implement cron job for  delete notifications

--- server/src/app/modules/Notification/notification.service.ts
+++ server/src/app/modules/Notification/notification.service.ts
@@ -1,5 +1,6 @@
 
 import { Notification } from "./notification.model";
+import cron from 'node-cron';
 
 
 const getNotificationsIntoDB = async () => {
@@ -13,6 +14,12 @@ const updateNotificationIntoDB = async (notificationId: string) => {
     const notifications = await Notification.find().sort({ createdAt: -1 });
     return notifications;
 }
+
+cron.schedule("0 0 0 * * *", async () => {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)   // 30 days ago;
+    await Notification.deleteMany({ status: 'read', createdAt: { $lt: thirtyDaysAgo } });
+    console.log('Cron job running every day at 00:00:00');
+}); // This will run every day at 00:00:00
 
 export const NotificationServices = {
     getNotificationsIntoDB,
