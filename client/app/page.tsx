7a62a9e139ebbcbb11a1104f2269e5aeb1b93a9a
raihanwebmaster <raihanemon2015@gmail.com>
2024-07-01 09:49:02 +0200

    create a header file

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,18 @@
 "use client"
 
-import React , {FC} from "react"
+import React , {FC, useState} from "react"
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
 interface Props{}
 
 const Page: FC<Props> = (props)  => {
+  const [open, setOpen] = useState(false)
+  const [activeItem, setActiveItem] = useState(0)
   return (
     <div>
       <Heading title="ECourses" description="ECourses is a platform for students to lear and get help from teachers" keywords="Programming,MERN,Redux,Machine Learning" />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
     </div>
   )
 }
