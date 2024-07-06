cda1dd401a3e45307a3c1488d4c7c50a0264525e
raihanwebmaster <raihanemon2015@gmail.com>
2024-07-06 23:45:38 +0200

    update navbar

--- client/app/utils/NavItems.tsx
+++ client/app/utils/NavItems.tsx
@@ -35,7 +35,7 @@ const NavItems: FC<Props> = ({ activeItem, isMobile }) => {
             <div className='hidden 800px:flex'>
                 {navItemsData && navItemsData.map((item, index) => (
                     <Link href={`${item.url}`} key={index}>
-                        <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson] " : " dark:text-white text-black "} text-[18px] px-6 font-Poppins font-[400]`}>
+                        <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson] " : " dark:text-white text-black "} text-[18px] px-6 font-Poppins font-[400] hover:dark:text-[#37a39a] hover:text-[crimson] `}>
                             {item.name}
                         </span>
                     </Link>
@@ -53,7 +53,7 @@ const NavItems: FC<Props> = ({ activeItem, isMobile }) => {
                     {
                         navItemsData && navItemsData.map((item, index) => (
                             <Link href='/' passHref key={index}>
-                                <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : " dark:text-white text-black"} block py-5 text-[18px] px-6 font-Poppins font-[400]`}>
+                                <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : " dark:text-white text-black"} block py-5 text-[18px] px-6 font-Poppins font-[400] hover:dark:text-[#37a39a] hover:text-[crimson]`}>
                                     {item.name}
                                 </span>
                             </Link>
