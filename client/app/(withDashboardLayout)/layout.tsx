09494e5b2e60dd6a401611e51ea67ef9b60261cc
raihanwebmaster <raihanemon2015@gmail.com>
2024-07-15 21:23:50 +0200

    update

--- client/app/(withDashboardLayout)/layout.tsx
+++ client/app/(withDashboardLayout)/layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                 <div className='1500px:w-[16%] w-1/5 ' >
                     <AdminSidebar />
                 </div>
-                <div className='w-[85%]' >
+                <div className='w-[80%] m-auto' >
                     <DashboardHero />
                     {children}
                 </div>
