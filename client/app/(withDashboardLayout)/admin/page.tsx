897014305b0d9f0905717eb4b64171670764c802
raihanwebmaster <raihanemon2015@gmail.com>
2024-07-18 17:04:04 +0200

    improved

--- client/app/(withDashboardLayout)/admin/page.tsx
+++ client/app/(withDashboardLayout)/admin/page.tsx
@@ -6,11 +6,10 @@ import React from 'react'
 type Props = {}
 
 const Page = (props: Props) => {
-  const [open, setOpen] = React.useState(false)
   return (
     <div>
       <Heading title="E-Courses - Admin" description="E-Courses is a platform for students to learn and get help from teachers" keywords="Programming,MERN,Redux,Machine Learing" />
-      <DashboardWidget open={open} />
+      <DashboardWidget />
     </div>
   )
 }
