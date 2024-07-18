241665a480851ac59ec3c2d05b16432c8196322f
raihanwebmaster <raihanemon2015@gmail.com>
2024-07-18 12:48:23 +0200

    implement orders analytics component

---
+++ client/app/(withDashboardLayout)/admin/orders-analytics/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React, { FC } from 'react'
+import Heading from '@/app/utils/Heading'
+import OrdersAnalytics from '@/app/components/Admin/Analytics/OrdersAnalytics'
+
+type Props = {}
+
+const Page:FC<Props> = () => {
+  return (
+    <div>
+        <Heading title="E-Courses - Admin" description="E-Courses is a platform for students to learn and get help from teachers" keywords="Programming,MERN,Redux,Machine Learing" />  
+        <OrdersAnalytics    />
+    </div>
+  )
+}
+
+export default Page
