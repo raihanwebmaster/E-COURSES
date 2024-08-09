5abda09c5002c0e01699ee18c14db1404218b149
raihanwebmaster <raihanemon2015@gmail.com>
2024-08-09 23:13:49 +0200

    SETUP FOR  MOBILE APP

---
+++ mobile-app-client/app/(routes)/onboarding/index.tsx
@@ -0,0 +1,12 @@
+import OnBoardingScreen from '@/app/screens/onboarding/onboarding.screen'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function OnBoarding() {
+  return (
+    <OnBoardingScreen>
+        
+    </OnBoardingScreen>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ mobile-app-client/app/_layout.tsx
@@ -0,0 +1,45 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import 'react-native-reanimated';
+
+import { View } from 'react-native';
+import OnBoarding from './(routes)/onboarding';
+
+export {
+  ErrorBoundary,
+} from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const [isLoggedIn, setisLoaggedIn] = useState(false);
+
+  return (
+       isLoggedIn ? <View></View> : <OnBoarding />
+  );
+}
